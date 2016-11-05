5c301706dde15a4e91b3b68887ab417abdfacc9e
earias <earias@ufs82.ufs.conae.gov.ar>
2016-11-05 13:23:11 -0300

    Nuevas funciones al Analyzer

--- Analyzer/Analyzer.py
+++ Analyzer/Analyzer.py
@@ -10,6 +10,27 @@ Módulo que analiza el código fuente
 from Reader import Reader
 from enum import Enum
 import string
+import numpy as np
+
+class campoCaracter:
+    
+    def __init__(self):
+        self.mVariable = np.array([['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'  ], #A
+                                    ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P' ], #R
+                                    ['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X' ], #G
+                                    ['Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f' ], #E
+                                    ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ], #N
+                                    ['o', 'p', 'q', 'r', 's', 't', 'u', 'v' ], #T
+                                    ['w', 'x', 'y', 'z',  1 ,  2 ,  3 ,  4  ], #O
+                                    [ 5 ,  6 ,  7 ,  8 ,  9 ,  0 ,  99, -99 ]  #S
+                                   ])        
+                                    # A    R    G    E    N    T    O     S
+    def DameCaracter (self, y, x, caballo):
+      self.y, self.x = caballo.damePosCaballo()
+      return self.mVariable[self.y][self.x]
+            
+        
+        
 
 class coor(Enum):
     """Coordenadas del tablero
@@ -30,7 +51,51 @@ class caballo:
         self.y = coor.s
         self.x = coor.r
     
-    def mover (self, y, x):
+    def dameCoordenadasEnNumero(self, yy,xx):
+        y = yy.lower()
+        x = xx.lower()
+        
+        #Coloca coordenada en y
+        if y == 'a':
+            y = coor.a
+        if y == 'r':
+            y = coor.r
+        if y == 'g':
+            y = coor.g
+        if y == 'e':
+            y = coor.e
+        if y == 'n':
+            y = coor.n
+        if y == 't':
+            y = coor.t
+        if y == 'o':
+            y = coor.o
+        if y == 's':
+            y = coor.s
+        
+        #Coloca coordenada en x
+        if x == 'a':
+            x = coor.a
+        if x == 'r':
+            x = coor.r
+        if x == 'g':
+            x = coor.g
+        if x == 'e':
+            x = coor.e
+        if x == 'n':
+            x = coor.n
+        if x == 't':
+            x = coor.t
+        if x == 'o':
+            x = coor.o
+        if x == 's':
+            x = coor.s
+        
+        return y, x 
+        
+    def mover (self, yy, xx):
+        y = yy.lower()
+        x = xx.lower()
         
         #Coloca coordenada en y
         if y == 'a':
@@ -67,17 +132,62 @@ class caballo:
             self.x = coor.o
         if x == 's':
             self.x = coor.s
-    
+           
+     
+    def damePosCaballo (self):
+           return self.y, self.x
         
     
-    def isCorrectMove(self, y, x):
-        return 1
+    def isCorrectMove(self, yy, xx):
+        """Funcion para saber si movimiento del caballo es correcto
+        
+        Parameters
+        ----------
+        
+        yy: int -> Es la nueva posición del caballo en Y
+        
+        xx: int -> Es la nueva posición del caballo en X
+        
+        Return
+        ------
+        
+        1 : correcto
+        
+        -1 : incorrecto
+        
+        """
+        yy, xx = self.dameCoordenadasEnNumero(yy, xx)    
+                
+        #Me aseguro que las nuevas posiciones no estén fuera del tablero
+        if xx > 7 or xx < 0:
+            #Fuera del tablero en x
+            return -1
+        if yy > 7 or yy < 0:
+            #Fuera del tablero en y
+            return -1   
+            
+        if ( ( self.x + 2 ) == xx ) or ( ( self.x - 2 ) == xx ):
+            #Significa que moví al caballo dos lugares a la derecha o a la izquierda
+            if ( ( self.y + 1 ) == yy ) or ( ( self.y + 1 ) == yy ):
+                #Significa que esta todo bien
+                return 1                
+                
+        elif ( ( self.y + 2 ) == yy ) or ( ( self.y - 2 ) == yy ):
+            #Significa que moví al caballo dos lugares Para abajo o arriba
+            if ( ( self.x + 1 ) == xx ) or ( ( self.x + 1 ) == xx ):
+                #Significa que esta todo bien
+                return 1    
+        else:
+            #Esta todo mal
+            return -1
 
 class Analyzer:
     def __init__(self):
         self.SourceCode = ""
         self.caballo = caballo()
-        
+        self.geFin = 0 #Variable para saber si el printeo ya termino
+        self.cCaracter = campoCaracter() 
+        self.sParaMostrarEnPantallita = ""
         
         """
         Estados 
@@ -136,41 +246,59 @@ class Analyzer:
         line = Reader.readLine(self.SourceFile)
         
         if(line != 'ARGENTOS\n'):
-            print "Error en la apertura del archivo"
+            print "\n\nMal pibe, no hiciste bien la apertura del archivo. Ponele ARGENTOS en el primer renglón"
             exit
         else:
             line = Reader.readLine(self.SourceFile)
             while(line != "ARGENTOS\n"):
-                line = string.split(Reader.readLine(self.SourceFile), " ")
+                line = string.split(string.split(line, "\n")[0], " ")
+                #line = string.split(string.split(Reader.readLine(self.SourceFile), "\n")[0], " ")
                 
-                #for i in range(len(line)):
                 i = 0 
-                while i < len(line[i]):
+                while i < len(line):
                     #Verifico si viene un comando de cambio de posición del caballo
                     if line[i] == "argento":
                         y = list(line[i+1])[0]
                         x = list(line[i+1])[1]
-                        self.caballo.mover(y, x)          
-                        #TODO: Verificar si es posible moverlo según regla de ajedrez
+                                
+                        #Verifico si es legal el movimiento del caballo
+                        if self.caballo.isCorrectMove(y,x) == -1:
+                            print "\n\nError Chabon: Moviste mal el caballo \n\n"
+                            exit(-1)
+                        
+                        #El movimiento del caballo es ok, por lo tanto continúo
+                        self.caballo.mover(y, x)    
+                        
                         i = i + 2 #Aumento el índice
                         if(y == 't' and x == 'a'):
                             #Ingreso al campo de caracteres
                             self.estado = 1
                         
-                    elif(line[i] == 'ge'):
+                    elif(line[i] == 'ge' and self.geFin == 0):
+                        #Se necesita imprimir
                         line = Reader.readLine(self.SourceFile)
-                        while(line != 'ge'):
-                            line = string.split(line, " ")
-                            
+                        line = string.split(string.split(line, "\n")[0]," " )
+                        while(line[0] != 'ge'):
                             j = 0
                             while j < len(line):
-                                #Me encuentro en el campo principal                                 
+                                #Me encuentro en el campo principal   
+                                if line[0] == '' and len(line) == 1:
+                                    line  = Reader.readLine(self.SourceFile)
+                                    line = string.split(string.split(line, "\n")[0]," " )
+                                    
                                 if self.estado == 0:
                                     if line[j] == "argento":
                                         y = list(line[j+1])[0]
                                         x = list(line[j+1])[1]
+                                        
+                                        #Verifico si es legal el movimiento del caballo
+                                        if self.caballo.isCorrectMove(y,x) == -1:
+                                            print "\n\nError Chabon: Moviste mal el caballo \n\n"
+                                            exit(-1)
+                                        
+                                        #El movimiento del caballo es ok, por lo tanto continúo                                            
                                         self.caballo.mover(y, x)          
-                                        #TODO: Verificar si es posible moverlo según regla de ajedrez
+
                                         j = j + 2 #Aumento el índice
                                         
                                     if(line[j] == "aaa"):
@@ -180,28 +308,53 @@ class Analyzer:
                                         if(y == 'T' and x == 'A'):
                                             #Ingreso al campo de caracteres
                                             self.estado = 1
+                                        if(y == 'N' and x == 'A'):
+                                            self.sParaMostrarEnPantallita += ' '
                                         break
+                                    
                                 
                                 #Me encuentro en el campo de caracteres
                                 elif self.estado == 1:
                                     if line[j] == "argento":
                                         y = list(line[j+1])[0]
                                         x = list(line[j+1])[1]
+                                        
+                                        #Verifico si es legal el movimiento del caballo
+                                        if self.caballo.isCorrectMove(y,x) == -1:
+                                            print "\n\nError Chabon: Moviste mal el caballo \n\n"
+                                            exit(-1)
+                                        
+                                        #El movimiento del caballo es ok, por lo tanto continúo   
                                         self.caballo.mover(y, x)          
-                                        #TODO: Verificar si es posible moverlo según regla de ajedrez
+
                                         j = j + 2 #Aumento el índice
                                         
-                                    if(line[j] == "aaa"):
+                                    elif(line[j] == "aaa"):
                                         y = list(line[j-1])[0]
                                         x = list(line[j-1])[1]
                                         
-                                        print y,x
-                                        #Me da el caracter
+                                        #Aquí leo en el campo de caracteres
+                                        self.sParaMostrarEnPantallita += self.cCaracter.DameCaracter(y,x, self.caballo)
+                                        j += 1
                                         
-                                    if(line[j] == "oo"):
+                                    elif(line[j] == "oo"):
                                         self.estado = 0                                         
                                         break
-                                
+                                    
+                                    elif (line[j] != "argento" and line[j] != "aaa"  and line[j] != "oo"):
+                                        j += 1
+                                    
+                            line  = Reader.readLine(self.SourceFile)
+                            line = string.split(string.split(line, "\n")[0]," " )
+                            
+                            #Para dar por finalizado el printeo                            
+                            if (line[0] == 'ge'):
+                                print self.sParaMostrarEnPantallita
+                                self.sParaMostrarEnPantallita = ""
+                                self.geFin = 1;
+                    i += 1
+                 
+                line = Reader.readLine(self.SourceFile)          
                                 
                                         
                                         
